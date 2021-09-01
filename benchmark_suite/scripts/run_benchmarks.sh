629c99d913a464c05228c330512d634a17c2cf1c
Captain Yoshi <captain.yoshisaur@gmail.com>
2021-08-31 21:15:29 -0400

    Fix rosparam argument in script

--- benchmark_suite/scripts/run_benchmarks.sh
+++ benchmark_suite/scripts/run_benchmarks.sh
@@ -2,10 +2,10 @@
 
 ### Example for running multiple benchmarks
 
-filename="bm"
+file="bm"
 
 for node in motion_planning collision_check; do
-    roslaunch moveit_benchmark_suite ${node}".launch" filename:=${filename}
+    roslaunch moveit_benchmark_suite ${node}".launch" output_file:=${file}
 done
 
 ### Output ~/.ros/bm.yaml containing 2 datasets (motion planning and collision check)
