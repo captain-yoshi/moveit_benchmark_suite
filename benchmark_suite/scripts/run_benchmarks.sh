08c668bfe192441220c6f9e292d62ef64521820d
Captain Yoshi <captain.yoshisaur@gmail.com>
2021-08-31 21:11:11 -0400

    Add comment about script output

--- benchmark_suite/scripts/run_benchmarks.sh
+++ benchmark_suite/scripts/run_benchmarks.sh
@@ -7,3 +7,5 @@ filename="bm"
 for node in motion_planning collision_check; do
     roslaunch moveit_benchmark_suite ${node}".launch" filename:=${filename}
 done
+
+### Output ~/.ros/bm.yaml containing 2 datasets (motion planning and collision check)
