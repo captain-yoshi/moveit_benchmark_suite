d6e4e4d050ece7f97ec62de160e1146246e3bc01
Captain Yoshi <captain.yoshisaur@gmail.com>
2021-12-10 16:17:03 -0500

    Add yaml node merge and comparators methods

--- benchmark_suite/include/moveit_benchmark_suite/yaml.h
+++ benchmark_suite/include/moveit_benchmark_suite/yaml.h
@@ -100,6 +100,85 @@
 
 #include <yaml-cpp/yaml.h>
 
+namespace moveit_benchmark_suite
+{
+// Taken from https://coryan.github.io/jaybeams/index.html
+// TODO liscence appropriatly
+
+/**
+ * Store the overrides for each class.
+ *
+ * Jaybeams configuration objects can be overriden "by-class",
+ * meaning, all configs of the same class receive the same overrides.
+ * This type is used to (temporarily) store the by-class overrides in
+ * a given context.
+ */
+typedef std::map<std::string, YAML::Node> class_overrides;
+
+namespace yaml
+{
+/**
+ * Merge two YAML nodes.
+ *
+ * Unlike a simple assignment, if @a source does not have a value
+ * for a given key, we keep the value from @a target.
+ */
+void merge_node(YAML::Node target, YAML::Node const& source);
+
+/**
+ * Merge all the values from @a source into @a target
+ *
+ * Unlike a simple assignment, if @a source does not have a value
+ * for a given key, we keep the value from @a target.
+ */
+void merge_map(YAML::Node target, YAML::Node const& source);
+
+/**
+ * Memberwise merge two sequences, from @a source into @a target.
+ *
+ * If @a source has more elements than @a target the additional values
+ * are appended.  If @a source has less elements than @a target, the
+ * extra values in @a target are unmodified.
+ */
+void merge_sequences(YAML::Node target, YAML::Node const& source);
+
+/**
+ * Merge the class-overrides from @a source into @a by_class.
+ *
+ * Given a set of by-class overrides apply any additional by-class
+ * overrides from @a source source into @a by_class.
+ */
+void merge(class_overrides& by_class, YAML::Node source);
+
+/**
+ * Recursively clone all the overrides in @a by_class.
+ */
+class_overrides clone(class_overrides const& by_class);
+
+// Modified version from https://gist.github.com/kunaltyagi/ebe13098cc22a717f793684659644f4e
+// TODO liscence appropriatly
+
+template <class... Args>
+bool scalar_compare(const YAML::Node&, const YAML::Node&);
+template <class T, class... Args>
+bool helper_compare(const YAML::Node& source, const YAML::Node& target)
+{
+  try
+  {
+    return source.as<T>() == target.as<T>();
+  }
+  catch (YAML::TypedBadConversion<T>& e)
+  {
+    return scalar_compare<Args...>(source, target);
+  }
+}
+
+bool isSubset(const YAML::Node& source, const YAML::Node& target);
+
+}  // namespace yaml
+
+}  // namespace moveit_benchmark_suite
+
 namespace YAML
 {
 template <>
--- benchmark_suite/src/yaml.cpp
+++ benchmark_suite/src/yaml.cpp
@@ -19,6 +19,154 @@
 
 using namespace moveit_benchmark_suite;
 
+void yaml::merge_node(YAML::Node target, YAML::Node const& source)
+{
+  switch (source.Type())
+  {
+    case YAML::NodeType::Scalar:
+      target = source.Scalar();
+      break;
+    case YAML::NodeType::Map:
+      merge_map(target, source);
+      break;
+    case YAML::NodeType::Sequence:
+      merge_sequences(target, source);
+      break;
+    case YAML::NodeType::Null:
+      throw std::runtime_error("merge_node: Null source nodes not supported");
+    case YAML::NodeType::Undefined:
+      throw std::runtime_error("merge_node: Undefined source nodes not supported");
+  }
+}
+
+void yaml::merge_map(YAML::Node target, YAML::Node const& source)
+{
+  for (auto const& j : source)
+  {
+    merge_node(target[j.first.Scalar()], j.second);
+  }
+}
+
+void yaml::merge_sequences(YAML::Node target, YAML::Node const& source)
+{
+  for (std::size_t i = 0; i != source.size(); ++i)
+  {
+    if (i < target.size())
+    {
+      merge_node(target[i], source[i]);
+    }
+    else
+    {
+      target.push_back(YAML::Clone(source[i]));
+    }
+  }
+}
+
+void yaml::merge(class_overrides& by_class, YAML::Node node)
+{
+  // Only Map nodes can override by-class values ...
+  if (not node.IsMap())
+  {
+    return;
+  }
+  // ... iterate over the node, searching for nodes with a key starting
+  // with ':' ...
+  for (auto i : node)
+  {
+    // ... the node is a map, there should be keys for all sub nodes ...
+    // std::runtime_error(i.first.as<std::string>("error on key"));
+    // ... found a key, check the format ...
+    std::string key = i.first.as<std::string>();
+    if (key[0] != ':')
+    {
+      continue;
+    }
+    // ... try to insert into the map ...
+    auto ins = by_class.emplace(key, i.second);
+    if (ins.second == true)
+    {
+      // ... good insert, nothing left to do ...
+      continue;
+    }
+    // ... okay there was a node for the class in the map already,
+    // need to merge the values ...
+    merge_node(ins.first->second, i.second);
+  }
+}
+
+class_overrides yaml::clone(class_overrides const& by_class)
+{
+  class_overrides tmp;
+  for (auto const& i : by_class)
+  {
+    tmp.emplace(i.first, YAML::Clone(i.second));
+  }
+  return tmp;
+}
+
+template <class... Args>
+bool yaml::scalar_compare(const YAML::Node& source, const YAML::Node& target)
+{
+  return helper_compare<Args...>(source, target);
+}
+template <>
+bool yaml::scalar_compare<>(const YAML::Node&, const YAML::Node&)
+{
+  return true;
+}
+
+bool yaml::isSubset(const YAML::Node& source, const YAML::Node& target)
+{
+  // Validation
+  // TODO validate Tag ? Not supported in yaml-cpp ...
+  if (source.Type() != target.Type())
+    return false;
+
+  switch (source.Type())
+  {
+    case YAML::NodeType::Scalar:
+      return scalar_compare<bool, int, double, std::string>(source, target);
+    case YAML::NodeType::Null:
+      return true;
+    case YAML::NodeType::Undefined:
+      return false;
+    case YAML::NodeType::Map:
+    case YAML::NodeType::Sequence:
+      if (source.size() > target.size())
+        return false;
+  }
+
+  // Loop through map
+  if (source.IsMap())
+  {
+    bool result = true;
+    for (YAML::const_iterator it1 = source.begin(), it2; it1 != source.end() && result == true; ++it1)
+    {
+      const auto& key = it1->first.as<std::string>();
+
+      if (target[key])
+        result = isSubset(it1->second, target[key]);
+      else
+        return false;
+    }
+    return result;
+  }
+
+  // Loop through sequence
+  if (source.IsSequence())
+  {
+    bool result = true;
+    for (YAML::const_iterator it1 = source.begin(), it2 = target.begin(); it1 != source.end() && result == true;
+         ++it1, ++it2)
+    {
+      result = isSubset(*it1, *it2);
+    }
+    return result;
+  }
+
+  return true;
+}
+
 namespace
 {
 // WARNING do not convert to/from u/int8_t (Explanation below)
