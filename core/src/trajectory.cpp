67e8321c62a3c431c6b7a397cec8ca4fe63ec005
Captain Yoshi <captain.yoshisaur@gmail.com>
2022-07-25 15:40:05 -0400

    [refac] trajectory: use ryml

--- core/src/trajectory.cpp
+++ core/src/trajectory.cpp
@@ -43,7 +43,10 @@ bool Trajectory::toYAMLFile(const std::string& filename) const
   moveit_msgs::RobotTrajectory msg;
   trajectory_->getRobotTrajectoryMsg(msg);
 
-  YAML::Node node = YAML::toNode(msg);
+  ryml::Tree t;
+  auto node = t.rootref();
+
+  node << msg;
   return IO::YAMLToFile(node, filename);
 }
 
