57c01992368ff686311e3a683406b336972f66e4
Captain Yoshi <captain.yoshisaur@gmail.com>
2022-08-15 14:14:42 -0400

    [refac] check collision when generating ik
    Changes:
    - Add `max_ik_solutions` parameter
    - Add optional `scene` parameter
    - Can visualize ik solutions

--- tools/src/ik_generator.cpp
+++ tools/src/ik_generator.cpp
@@ -1,13 +1,19 @@
 #include <ros/ros.h>
+
+#include <moveit/collision_detection/collision_common.h>
 #include <moveit/kinematics_base/kinematics_base.h>
 #include <moveit_benchmark_suite/resource_builder.h>
+#include <moveit_benchmark_suite/tools/rviz_visualization.h>
+#include <moveit_benchmark_suite/utils.h>
 
 using namespace moveit_benchmark_suite;
 
 constexpr char CONFIG_PARAMETER[] = "config_file";
+constexpr char VISUALIZE_PARAMETER[] = "visualize";
 
-bool getParamsFromConfig(const std::string& filename, std::string& group_name, std::vector<double>& ik_seed_state,
-                         geometry_msgs::Pose& pose, RobotPtr& robot);
+bool getParamsFromConfig(const std::string& filename, std::string& group_name, std::size_t& max_ik_solutions,
+                         std::vector<double>& ik_seed_state, geometry_msgs::Pose& pose, RobotPtr& robot,
+                         ScenePtr& scene);
 
 int main(int argc, char** argv)
 {
@@ -17,17 +23,20 @@ int main(int argc, char** argv)
 
   ros::NodeHandle pnh("~");
 
-  // Load config
+  // Load parameters
   std::string filename;
+  bool visualize;
   pnh.getParam(CONFIG_PARAMETER, filename);
+  pnh.getParam(VISUALIZE_PARAMETER, visualize);
 
   // Get parameters
   std::string group_name;
+  std::size_t max_ik_solutions = 1;
   std::vector<double> ik_seed_state;
   geometry_msgs::Pose pose;
   RobotPtr robot;
-
-  if (!getParamsFromConfig(filename, group_name, ik_seed_state, pose, robot))
+  ScenePtr scene;
+  if (!getParamsFromConfig(filename, group_name, max_ik_solutions, ik_seed_state, pose, robot, scene))
     return 0;
 
   auto jmg = robot->getModel()->getJointModelGroup(group_name);
@@ -37,42 +46,97 @@ int main(int argc, char** argv)
     return 0;
   }
 
-  // Compute IK
-  std::vector<std::vector<double>> solutions;
-  kinematics::KinematicsResult result;
-  kinematics::KinematicsQueryOptions options;
-  options.discretization_method = kinematics::DiscretizationMethod::NO_DISCRETIZATION;
+  // create seed state, defaults to config then generates random seed
+  robot_state::RobotState seed_state = robot_state::RobotState(robot->getModel());
+  for (std::size_t i = 0; i < ik_seed_state.size(); ++i)
+    seed_state.setVariablePosition(i, ik_seed_state[i]);
 
-  auto solver = jmg->getSolverInstance();
-  solver->getPositionIK({ pose }, ik_seed_state, solutions, result, options);
+  // prepare visualization
+  std::unique_ptr<tools::RVIZVisualization> rviz;
+  if (visualize)
+    rviz = std::make_unique<tools::RVIZVisualization>();
 
-  if (result.kinematic_error == kinematics::KinematicError::OK)
-    ROS_INFO("Kinematic Success");
-  else
+  // compute multiple ik
+  for (std::size_t ik_ctr = 0; ik_ctr < max_ik_solutions; ++ik_ctr)
   {
-    ROS_ERROR("Kinematic error code: %d", static_cast<int>(result.kinematic_error));
-    return 0;
-  }
+    std::vector<std::vector<double>> solutions;
+    kinematics::KinematicsResult result;
+    kinematics::KinematicsQueryOptions options;
+    options.discretization_method = kinematics::DiscretizationMethod::NO_DISCRETIZATION;
 
-  std::size_t solution_ctr = 0;
-  for (const auto& solution : solutions)
-  {
-    ROS_INFO("============");
-    ROS_INFO("Solution #%zu", solution_ctr);
-    ROS_INFO("============");
+    auto solver = jmg->getSolverInstance();
+    solver->getPositionIK({ pose }, ik_seed_state, solutions, result, options);
 
-    for (const auto& joint : solution)
+    if (result.kinematic_error != kinematics::KinematicError::OK)
     {
-      ROS_INFO("Joint = %f", joint);
+      ROS_ERROR("Kinematic error code: %d", static_cast<int>(result.kinematic_error));
+      return 0;
     }
-    solution_ctr++;
-  }
 
+    for (const auto& solution : solutions)
+    {
+      // set scene robot state from ik solution
+      auto& scene_state = scene->getCurrentState();
+      for (std::size_t i = 0; i < solution.size(); ++i)
+        scene_state.setVariablePosition(i, solution[i]);
+      scene_state.update();
+
+      // check if ik solution is in contact
+      bool in_collision = false;
+      collision_detection::CollisionRequest req;
+      collision_detection::CollisionResult res;
+      req.contacts = true;
+      req.max_contacts = 1;
+
+      scene->getScene()->checkCollision(req, res);
+
+      if (res.collision)
+      {
+        res.print();
+        in_collision = true;
+      }
+
+      // visualize
+      if (rviz)
+      {
+        rviz->initialize(robot, scene);
+
+        rviz->updateScene(scene->getScene(), scene->getCurrentState());
+        rviz->updateMarkers();
+
+        ROS_INFO("Press `enter` to view next query");
+        waitForKeyPress();
+
+        rviz->removeAllMarkers();
+        rviz->removeScene();
+      }
+
+      // print ik
+      if (!in_collision)
+      {
+        ROS_INFO("============");
+        ROS_INFO("Solution #%zu", ik_ctr);
+        ROS_INFO("============");
+
+        for (const auto& joint : solution)
+          ROS_INFO("Joint = %f", joint);
+      }
+    }
+
+    // change seed state
+    seed_state.setToRandomPositions();
+    seed_state.update();
+    auto joint_names = seed_state.getVariableNames();
+
+    for (std::size_t i = 0; i < joint_names.size(); ++i)
+      ik_seed_state[i] = seed_state.getVariablePosition(joint_names[i]);
+  }
   return 0;
 }
 
-bool getParamsFromConfig(const std::string& filename, std::string& group_name, std::vector<double>& ik_seed_state,
-                         geometry_msgs::Pose& pose, RobotPtr& robot)
+bool getParamsFromConfig(const std::string& filename, std::string& group_name, std::size_t& max_ik_solutions,
+                         std::vector<double>& ik_seed_state, geometry_msgs::Pose& pose, RobotPtr& robot,
+                         ScenePtr& scene)
 {
   ryml::Tree tree;
   ryml::NodeRef node = tree.rootref();
@@ -104,6 +168,7 @@ bool getParamsFromConfig(const std::string& filename, std::string& group_name, s
     ROS_WARN("Missing 'ik_seed_state' key");
     return false;
   }
+  n_config["max_ik_solutions"] >> max_ik_solutions;
   n_config["ik_seed_state"] >> ik_seed_state;
 
   // Build robot from YAML
@@ -124,6 +189,26 @@ bool getParamsFromConfig(const std::string& filename, std::string& group_name, s
       ROS_WARN("Cannot load kinematics plugin with group name '%s'", group_name.c_str());
       return false;
     }
+  }
+
+  // Build scene from YAML
+  if (n_config.has_child("scene"))
+  {
+    SceneBuilder builder;
+    builder.loadResources(n_config["scene"]);
+
+    auto map = builder.generateResources(robot, "FCL");
+
+    if (map.empty())
+    {
+      ROS_WARN("Scene failed to build...");
+      return false;
+    }
+    scene = map.begin()->second;
+  }
+  else
+  {
+    scene = std::make_shared<Scene>(robot->getModel());
   }
 
   // Build pose from YAML
